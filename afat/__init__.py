17417b469ef9ad9a83102367940c3318d3d0e4a5
Peter Pfeufer <ppfeufer@users.noreply.github.com>
2020-09-13 01:21:53 +0200

    Update __init__.py
    Another try to figure out what pylint wants

--- afat/__init__.py
+++ afat/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+__all__ = ['admin', 'apps', 'auth_hooks', 'forms',
+           'models', 'providers', 'tasks', 'tests', 'urls', 'utils', 'views']
+
 default_app_config = "afat.apps.AfatConfig"
 
 __title__ = "Fleet Activity Tracking"
