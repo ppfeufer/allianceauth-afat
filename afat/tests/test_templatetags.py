05cb4f95dd2f478bdaca990ba0026f34e986b0c8
Peter Pfeufer <info@ppfeufer.de>
2022-01-04 15:10:13 +0100

    Break browser cache on app update for selected static files

---
+++ afat/tests/test_templatetags.py
@@ -0,0 +1,44 @@
+"""
+Test our template tags
+"""
+
+# Django
+from django.template import Context, Template
+from django.test import TestCase
+
+# Alliance Auth AFAT
+from afat import __version__
+
+
+class TestAfatFilters(TestCase):
+    """
+    Test template filters
+    """
+
+    def test_month_name_filter(self):
+        context = Context({"month": 5})
+        template_to_render = Template("{% load filters %} {{ month|month_name }}")
+
+        rendered_template = template_to_render.render(context)
+
+        self.assertInHTML("May", rendered_template)
+
+
+class TestAfatVersionedStatic(TestCase):
+    """
+    Test versioned static template tag
+    """
+
+    def test_versioned_static(self):
+        context = Context({"version": __version__})
+        template_to_render = Template(
+            "{% load afat_versioned_static %}"
+            "{% afat_static 'afat/css/allianceauth-afat.min.css' %}"
+        )
+
+        rendered_template = template_to_render.render(context)
+
+        self.assertInHTML(
+            f'/static/afat/css/allianceauth-afat.min.css?v={context["version"]}',
+            rendered_template,
+        )
