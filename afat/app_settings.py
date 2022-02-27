de0b5d609d4d0ef96f42abc7138f1651823a4e0c
Peter Pfeufer <info@ppfeufer.de>
2022-02-27 15:56:12 +0100

    [FIX] cannot import name 'ugettext_lazy' from 'django.utils.translation'

--- afat/app_settings.py
+++ afat/app_settings.py
@@ -7,7 +7,7 @@ import unidecode
 
 # Django
 from django.utils.text import slugify
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Alliance Auth (External Libs)
 from app_utils.django import clean_setting
