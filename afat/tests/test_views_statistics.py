91de96e2c34957d51d1a48aad0320b6097159581
Peter Pfeufer <info@ppfeufer.de>
2022-04-25 03:06:35 +0200

    Tests added

--- afat/tests/test_views_statistics.py
+++ afat/tests/test_views_statistics.py
@@ -221,27 +221,79 @@ class TestStatistics(TestCase):
         # then
         self.assertEqual(res.status_code, 200)
 
-    # def test_should_show_character_stats_for_user_with_stats_char_other(self):
-    #     # given
-    #     self.client.force_login(self.user_with_stats_corporation_other)
-    #
-    #     # when
-    #     url = reverse(
-    #         "afat:statistics_character",
-    #         kwargs={
-    #             "charid": self.user_with_basic_access.profile.main_character.character_id,
-    #             "year": 2020,
-    #             "month": 4,
-    #         },
-    #     )
-    #     res = self.client.get(url)
-    #
-    #     # then
-    #     self.assertEqual(res.status_code, 200)
+    def test_should_show_character_stats_for_user_with_stats_corporation_own(self):
+        # given
+        self.client.force_login(self.user_with_stats_corporation_own)
+
+        # when
+        url = reverse(
+            "afat:statistics_character",
+            kwargs={
+                "charid": self.user_with_basic_access.profile.main_character.character_id,
+                "year": 2020,
+                "month": 4,
+            },
+        )
+        res = self.client.get(url)
+
+        # then
+        self.assertEqual(res.status_code, 200)
+
+    def test_should_show_character_stats_for_user_with_stats_corporation_other(self):
+        # given
+        self.client.force_login(self.user_with_stats_corporation_other)
+
+        # when
+        url = reverse(
+            "afat:statistics_character",
+            kwargs={
+                "charid": self.user_with_basic_access.profile.main_character.character_id,
+                "year": 2020,
+                "month": 4,
+            },
+        )
+        res = self.client.get(url)
+
+        # then
+        self.assertEqual(res.status_code, 200)
+
+    def test_should_not_show_other_character_stats_for_user(self):
+        # given
+        self.client.force_login(self.user_with_basic_access)
+
+        # when
+        url = reverse(
+            "afat:statistics_character",
+            kwargs={
+                "charid": self.user_with_stats_corporation_other.profile.main_character.character_id,
+                "year": 2020,
+                "month": 4,
+            },
+        )
+        res = self.client.get(url)
+
+        # then
+        self.assertEqual(res.status_code, 302)
 
     def test_should_show_own_corp_stats_for_user_with_stats_corporation_own(self):
         # given
         self.client.force_login(self.user_with_stats_corporation_own)
+
+        # when
+        url = reverse(
+            "afat:statistics_corporation",
+            kwargs={
+                "corpid": self.user_with_stats_corporation_other.profile.main_character.corporation_id
+            },
+        )
+        res = self.client.get(url)
+
+        # then
+        self.assertEqual(res.status_code, 200)
+
+    def test_should_show_own_corp_stats_for_user_with_stats_corporation_other(self):
+        # given
+        self.client.force_login(self.user_with_stats_corporation_other)
 
         # when
         url = reverse(
@@ -254,6 +306,22 @@ class TestStatistics(TestCase):
 
         # then
         self.assertEqual(res.status_code, 200)
+
+    def test_should_not_show_own_corp_stats_for_user(self):
+        # given
+        self.client.force_login(self.user_with_basic_access)
+
+        # when
+        url = reverse(
+            "afat:statistics_corporation",
+            kwargs={
+                "corpid": self.user_with_stats_corporation_own.profile.main_character.corporation_id
+            },
+        )
+        res = self.client.get(url)
+
+        # then
+        self.assertEqual(res.status_code, 302)
 
     def test_should_show_all_corp_stats_for_user_with_stats_corporation_other(self):
         # given
