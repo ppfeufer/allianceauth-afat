8a1f22b1f2fced6e70eccd0ad67db393e28415d8
Peter Pfeufer <info@ppfeufer.de>
2024-02-25 15:14:03 +0100

    [MISC] Switched to dynamic version

--- afat/__init__.py
+++ afat/__init__.py
@@ -2,14 +2,8 @@
 App config
 """
 
-# Standard Library
-from importlib import metadata
-
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = metadata.version(distribution_name="allianceauth-afat")
-
-del metadata
-
+__version__ = "2.13.0"
 __title__ = _("Fleet Activity Tracking")
