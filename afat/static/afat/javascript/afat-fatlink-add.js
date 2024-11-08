58cf56dc5dc3f3938931611649b82e0ccde7cb2c
Peter Pfeufer <info@ppfeufer.de>
2024-11-08 18:46:29 +0100

    [FIX] Autocomplete error when no doctrine is available

--- afat/static/afat/javascript/afat-fatlink-add.js
+++ afat/static/afat/javascript/afat-fatlink-add.js
@@ -3,21 +3,31 @@ import Autocomplete from '/static/afat/libs/bootstrap5-autocomplete/1.1.25/autoc
 $(document).ready(() => {
     'use strict';
 
+    /**
+     * Initialize autocomplete dropdown
+     *
+     * @param {HTMLElement} element
+     */
     const autoCompleteDropdown = (element) => {
-        const autoCompleteDoctrine = new Autocomplete( // eslint-disable-line no-unused-vars
-            element,
-            Object.assign(
-                {},
-                {
-                    onSelectItem: console.log,
-                },
-                {
-                    onRenderItem: (item, label) => {
-                        return `<l-i set="fl" name="${item.value.toLowerCase()}" size="16"></l-i> ${label}`;
+        const datalistId = element.getAttribute('data-datalist');
+        const datalist = document.getElementById(datalistId);
+
+        if (datalist) {
+            const autoCompleteDoctrine = new Autocomplete( // eslint-disable-line no-unused-vars
+                element,
+                Object.assign(
+                    {},
+                    {
+                        onSelectItem: console.log,
                     },
-                }
-            )
-        );
+                    {
+                        onRenderItem: (item, label) => {
+                            return `<l-i set="fl" name="${item.value.toLowerCase()}" size="16"></l-i> ${label}`;
+                        },
+                    }
+                )
+            );
+        }
     };
 
     autoCompleteDropdown(document.getElementById('id_doctrine_esi'));
