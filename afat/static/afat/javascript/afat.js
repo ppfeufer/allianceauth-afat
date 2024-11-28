3c6ea2aedaf1d7910378a7abbce523b534333c05
Peter Pfeufer <info@ppfeufer.de>
2024-11-28 02:51:09 +0100

    [ADD] Overridable default JS options

--- afat/static/afat/javascript/afat.js
+++ afat/static/afat/javascript/afat.js
@@ -1,9 +1,49 @@
+/* global afatJsSettingsOverride, afatJsSettingsDefaults */
+
+// Build the settings object
+let afatSettings = afatJsSettingsDefaults;
+if (typeof afatJsSettingsOverride !== 'undefined') {
+    afatSettings = Object.assign(
+        {},
+        afatJsSettingsDefaults,
+        afatJsSettingsOverride
+    );
+}
+
 /**
  * Datetime format for AFAT
  *
  * @type {string}
  */
-const AFAT_DATETIME_FORMAT = 'YYYY-MMM-DD, HH:mm'; // eslint-disable-line no-unused-vars
+const AFAT_DATETIME_FORMAT = afatSettings.datetimeFormat; // eslint-disable-line no-unused-vars
+
+/**
+ * Fetch data from an ajax URL
+ *
+ * @param {string} url The URL to fetch data from
+ * @returns {Promise<any>} The fetched data
+ */
+const fetchAjaxData = async (url) => { // eslint-disable-line no-unused-vars
+    'use strict';
+
+    return await fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return Promise.resolve(response);
+            } else {
+                return Promise.reject(new Error('Failed to load'));
+            }
+        })
+        .then(response => {
+            response.json();
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(function (error) {
+            console.log(`Error: ${error.message}`);
+        });
+};
 
 /**
  * Convert a string to a slug
