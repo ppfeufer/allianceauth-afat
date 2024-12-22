2faa2a026d9d45c71eaea66cbb6d78da6e55cc1d
Peter Pfeufer <info@ppfeufer.de>
2024-12-22 23:15:13 +0100

    [CHANGE] Make pylint happy
    Question is: Why out of a sudden

--- afat/urls/__init__.py
+++ afat/urls/__init__.py
@@ -7,23 +7,20 @@ from django.urls import include, path
 
 # Alliance Auth AFAT
 from afat.constants import INTERNAL_URL_PREFIX
-from afat.urls.ajax import urls as ajax_urls
-from afat.urls.dashboard import urls as dashboard_urls
-from afat.urls.fatlinks import urls as fatlinks_urls
-from afat.urls.logs import urls as logs_urls
-from afat.urls.statistics import urls as statistics_urls
+from afat.urls import statistics  # pylint: disable=W0406 E0611
+from afat.urls import ajax, dashboard, fatlinks, logs
 
 app_name: str = "afat"
 
 urlpatterns = [
     # Dashboard
-    path(route="", view=include(dashboard_urls)),
+    path(route="", view=include(dashboard.urls)),
     # Log urls
-    path(route="logs/", view=include(logs_urls)),
+    path(route="logs/", view=include(logs.urls)),
     # FAT Links urls
-    path(route="fatlinks/", view=include(fatlinks_urls)),
+    path(route="fatlinks/", view=include(fatlinks.urls)),
     # Statistics urls
-    path(route="statistics/", view=include(statistics_urls)),
+    path(route="statistics/", view=include(statistics.urls)),
     # Ajax calls urls
-    path(route=f"{INTERNAL_URL_PREFIX}/ajax/", view=include(ajax_urls)),
+    path(route=f"{INTERNAL_URL_PREFIX}/ajax/", view=include(ajax.urls)),
 ]
